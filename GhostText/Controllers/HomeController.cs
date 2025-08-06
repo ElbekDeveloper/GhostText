d5f52bb83e02f231814880f136919884a45b6506
fwAlphA <boburbek200101@gmail.com>
2025-08-06 14:10:39 +0500

    Create Controller

--- GhostText/Controllers/HomeController.cs
+++ GhostText/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GhostText.Models;
+using GhostText.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GhostText.Controllers
 {
@@ -6,10 +8,25 @@ namespace GhostText.Controllers
     [Route("api/[controller]")]
     public class HomeController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult GetHome() 
+        private readonly IMessageService messageService;
+
+        public HomeController(IMessageService messageService)
         {
-            return Content("Hello, World!");
+            this.messageService = messageService;
+        }
+
+        [HttpPost]
+        public async Task<Message> PostMessageAsync(Message message) 
+        {
+                await this.messageService.AddMessageAsync(message);
+                return message;
+        }
+
+        [HttpGet]
+        public IQueryable<Message> GetAllMessages()
+        {
+                IQueryable<Message> messages = this.messageService.RetrieveAllMessage();
+                return messages;
         }
     }
 }
