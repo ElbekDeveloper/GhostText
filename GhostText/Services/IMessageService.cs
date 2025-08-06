791d8aae9f813511fb0654c71bc46460ffae48d0
Mustafo6252 <ravshanovmustafo88@gmail.com>
2025-08-06 13:57:44 +0500

    creating Services fully ended

--- GhostText/Services/IMessageService.cs
+++ GhostText/Services/IMessageService.cs
@@ -5,7 +5,14 @@ namespace GhostText.Services
     public interface IMessageService
     {
         Task<Message> AddMessageAsync(Message message);
+        
         IQueryable<Message> RetrieveAllMessage();
+        
+        Task<Message> RetrieveMessageByYdAsync(Guid messageId);
+        
+        Task<Message> ModifyMessageAsync(Message message);
+        
+        Task<Message> DeleteMessageAsync(Guid messageId);
 
     }
 }
--- GhostText/Services/MessageService.cs
+++ GhostText/Services/MessageService.cs
@@ -26,5 +26,40 @@ namespace GhostText.Services
             var messages = this.messageRepository.SelectAllMessages();
             return messages;
         }
+
+        public async Task<Message> RetrieveMessageByYdAsync(Guid messageId)
+        {
+            Message message= await this.messageRepository.SelectMessageById(messageId);
+            if (message == null)
+            {
+                throw new KeyNotFoundException($"Message with ID {messageId} not found");
+            }
+            
+            return message;
+        }
+
+        public async Task<Message> ModifyMessageAsync(Message message)
+        {
+            Message maybeMessage=await this.messageRepository.SelectMessageById(message.Id);
+            if (maybeMessage == null)
+            {
+                throw new KeyNotFoundException($"Message with ID {message.Id} not found");
+            }
+            
+            await this.messageRepository.UpdateMessageAsync(message);
+            return message;
+        }
+
+        public async Task<Message> DeleteMessageAsync(Guid messageId)
+        {
+            Message maybeMessage = await this.messageRepository.SelectMessageById(messageId);
+            if (maybeMessage == null)
+            {
+                throw new KeyNotFoundException($"Message with ID {messageId} not found");
+            }
+            
+            await this.messageRepository.DeleteMessageAsync(maybeMessage);
+            return maybeMessage;
+        }
     }
 }
