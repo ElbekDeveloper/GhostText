db3e60c84dd775862103547d4f478c0aadadb3bd
Mustafo6252 <ravshanovmustafo88@gmail.com>
2025-08-06 13:16:20 +0500

    updated and deletet repositories created

--- GhostText/Repositories/IMessageRepository.cs
+++ GhostText/Repositories/IMessageRepository.cs
@@ -5,6 +5,12 @@ namespace GhostText.Repositories;
 public interface IMessageRepository
 {
     Task<Message> InsertMessageAsync(Message message);
+    
     IQueryable<Message> SelectAllMessages();
+    
     Task<Message> SelectMessageById(Guid Id);
+    
+    Task<Message>UpdateMessageAsync(Message message);
+    
+    Task<Message> DeleteMessageAsync(Message message);
 }
--- GhostText/Repositories/MessageRepository.cs
+++ GhostText/Repositories/MessageRepository.cs
@@ -22,15 +22,29 @@ public class MessageRepository : IMessageRepository
 
     public IQueryable<Message> SelectAllMessages()
     {
-        IQueryable<Message> messages = this.applicationDbContext.Messages;
-
+        IQueryable<Message> messages = 
+            this.applicationDbContext.Messages;
         return messages;
     }
 
-    public async Task<Message> SelectMessageById(Guid Id)
+    public async Task<Message> SelectMessageById(Guid id)
     {
-        var result = await this.applicationDbContext.Messages.FirstOrDefaultAsync(x => x.Id == Id);
-
+        var result = 
+            await this.applicationDbContext.Messages.FirstOrDefaultAsync(x => x.Id == id);
         return result;
+    }
+
+    public async Task<Message> UpdateMessageAsync(Message message)
+    {
+        this.applicationDbContext.Messages.Update(message);
+        await this.applicationDbContext.SaveChangesAsync();
+        return message;
+    }
+
+    public async Task<Message> DeleteMessageAsync(Message message)
+    {
+        this.applicationDbContext.Messages.Remove(message);
+        await this.applicationDbContext.SaveChangesAsync();
+        return message;
     }
 }
