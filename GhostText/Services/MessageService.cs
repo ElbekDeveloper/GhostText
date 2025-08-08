662482293a764e34e5a5673e21052b3b14e47ab1
fwAlphA <boburbek200101@gmail.com>
2025-08-08 18:39:46 +0500

    Update - Master

--- GhostText/Services/MessageService.cs
+++ GhostText/Services/MessageService.cs
@@ -21,6 +21,22 @@ namespace GhostText.Services
             return await this.messageRepository.InsertMessageAsync(message);
         }
 
+        public IQueryable<Message> RetrieveAllMessages()
+        {
+            return this.messageRepository.SelectAllMessages();
+        }
+
+        public async Task<Message> RetrieveMessageByIdAsync(Guid messageId)
+        {
+            var message = await this.messageRepository.SelectMessageByIdAsync(messageId);
+            if (message is null)
+            {
+                throw new KeyNotFoundException($"Message with Id:{messageId} is not found");
+            }
+
+            return message;
+        }
+
         public async Task<Message> ModifyMessageAsync(Message message)
         {
             if (message is null)
@@ -30,25 +46,11 @@ namespace GhostText.Services
 
             var existingMessage = await this.messageRepository.SelectMessageByIdAsync(message.Id);
 
-            if (existingMessage is null) { 
+            if (existingMessage is null) {
                 throw new KeyNotFoundException("Message not found");
             }
 
             return await this.messageRepository.UpdateMessageAsync(message);
-
-        public async Task<Message> RetrieveMessageByIdAsync(Guid messageId)
-        {
-            var message=await this.messageRepository.SelectMessageByIdAsync(messageId);
-            if (message == null)
-            {
-                throw new KeyNotFoundException($"Message with Id:{messageId} is not found");
-            }
-            
-            return message;
-
-        public IQueryable<Message> RetrieveAllMessages()
-        {
-            return this.messageRepository.SelectAllMessages();
         }
     }
 }
