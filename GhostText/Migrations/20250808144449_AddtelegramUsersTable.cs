444ed43627d7594dab2ac3aba15ad16705c56482
Oybek Nematov <1377oybeknematov1377@gmail.com>
2025-08-08 19:54:50 +0500

    migrate telegram users

---
+++ GhostText/Migrations/20250808144449_AddtelegramUsersTable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GhostText.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddtelegramUsersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TelegramUsers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserName = table.Column<string>(type: "text", nullable: true),
+                    FullName = table.Column<string>(type: "text", nullable: true),
+                    TelegramId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TelegramUsers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TelegramUsers");
+        }
+    }
+}
