8cb7e00a83b79ee559f557ef8d9343757ab60679
Oybek Nematov <1377oybeknematov1377@gmail.com>
2025-08-08 18:58:04 +0500

    create post controller

--- GhostText/Program.cs
+++ GhostText/Program.cs
@@ -1,4 +1,6 @@
 using GhostText.Data;
+using GhostText.Repositories;
+using GhostText.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -11,14 +13,15 @@ var builder = WebApplication.CreateBuilder(args);
 string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseNpgsql(connectionString));
-
+builder.Services.AddScoped<IMessageRepository, MessageRepository>();
+builder.Services.AddScoped<IMessageService, MessageService>();
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
-{
+{    
     app.MapOpenApi();
     app.MapScalarApiReference();
 }
