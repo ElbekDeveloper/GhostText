2782e1289feb8d083949643fd72a4516330e4f43
AloidinAkramov <aloidin.akram@gmail.com>
2025-08-15 04:56:41 +0500

    TelegramClient

---
+++ GhostText/Models/TelegramSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace GhostText.Models
+{
+    public class TelegramSettings
+    {
+        public string BotToken { get; set; }
+        public string ChannelId { get; set; }
+    }
+}
---
+++ GhostText/TelegramClient/TelegramClient.cs
@@ -0,0 +1,26 @@
+﻿using GhostText.Models;
+using Microsoft.Extensions.Options;
+using Telegram.Bot;
+
+namespace GhostText.TelegramClient
+{
+    public class TelegramClient
+    {
+        private readonly TelegramSettings telegramSettings;
+        private readonly TelegramBotClient botClient;
+
+        public TelegramClient(IOptions<TelegramSettings> options)
+        {
+            telegramSettings = options.Value;
+            botClient = new TelegramBotClient(telegramSettings.BotToken);
+        }
+
+        //public void ListenTelegramBot()
+        //{
+        //    botClient.StartReceiving(
+               
+        //        );
+        //}
+
+    }
+}
