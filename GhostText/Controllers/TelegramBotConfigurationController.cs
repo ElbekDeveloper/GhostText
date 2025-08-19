2e3bb52818ead105f020b6481f40e803796f2671
fwAlphA <boburbek200101@gmail.com>
2025-08-19 19:47:05 +0500

    add Controller PostTelegramBotConfigurationAsync and fix AddTransient

---
+++ GhostText/Controllers/TelegramBotConfigurationController.cs
@@ -0,0 +1,22 @@
+﻿using GhostText.Models.TelegramBotConfiguration;
+using GhostText.Services.TelegramBotConfigurations;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GhostText.Controllers
+{
+    [ApiController]
+    [Route("api/telegram-bot-configuration")]
+    public class TelegramBotConfigurationController : ControllerBase
+    {
+        private readonly ITelegramBotConfigurationService telegramBotConfigurationService;
+
+        public TelegramBotConfigurationController(ITelegramBotConfigurationService telegramBotConfigurationService) =>
+            this.telegramBotConfigurationService = telegramBotConfigurationService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<TelegramBotConfiguration>> PostTelegramBotConfigurationAsync(
+            TelegramBotConfiguration configuration) =>
+            Ok(await this.telegramBotConfigurationService.InsertChannelAsync(configuration));
+    }
+}
