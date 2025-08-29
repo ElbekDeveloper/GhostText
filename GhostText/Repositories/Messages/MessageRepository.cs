ea68d767d1fa3ff2e91a55e0671a82bad7dad2ca
Oybek Nematov <1377oybeknematov1377@gmail.com>
2025-08-29 17:48:42 +0500

    Decrypt

--- GhostText/Repositories/Messages/MessageRepository.cs
+++ GhostText/Repositories/Messages/MessageRepository.cs
@@ -1,10 +1,11 @@
-using System;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using GhostText.Data;
 using GhostText.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace GhostText.Repositories
 {
---
+++ GhostText/Services/Shifr/Shifr.cs
@@ -0,0 +1,8 @@
+﻿namespace GhostText.Services.Shifr
+{
+    public class Shifr : IShifr
+    {
+        public string Decrypt { get => throw new System.NotImplementedException(); 
+            set => throw new System.NotImplementedException(); }
+    }
+}
