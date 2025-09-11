7686cf25fd87b89969d9ddd54f539852e4c50e1b
Nodir Abdumurotov <nabdumurotov@amcbridge.com>
2025-09-11 20:03:54 +0500

    ShouldAddMessageAsync -> PASS

---
+++ GhostText.Unit.Tests/Services/Messages/MessageServiceTests.Logic.Add.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Force.DeepCloner;
+using GhostText.Models;
+using Moq;
+
+namespace GhostText.Unit.Tests.Services.Messages
+{
+    public partial class MessageServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddMessageAsync()
+        {
+            // given
+            Message randomMessage = CreateRandomMessage();
+            Message inputMessage = randomMessage;
+            Message persistedMessage = inputMessage;
+            Message expectedMessage = persistedMessage.DeepClone();
+
+            this.messageRepositoryMock.Setup(repository =>
+                repository.InsertMessageAsync(inputMessage))
+                    .ReturnsAsync(persistedMessage);
+
+            // when
+            Message actualMessage =
+                await this.messageService.AddMessageAsync(inputMessage);
+
+            // then
+            actualMessage.Should().BeEquivalentTo(expectedMessage, option =>
+                option.Excluding(message => message.CreateDate));
+
+            this.messageRepositoryMock.Verify(repository =>
+                repository.InsertMessageAsync(inputMessage),
+                    Times.Never);
+
+            this.messageRepositoryMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ GhostText.Unit.Tests/Services/Messages/MessageServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using GhostText.Models;
+using GhostText.Repositories;
+using GhostText.Services;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace GhostText.Unit.Tests.Services.Messages
+{
+    public partial class MessageServiceTests
+    {
+        private readonly IMessageService messageService;
+        private readonly Mock<IMessageRepository> messageRepositoryMock;
+
+        public MessageServiceTests()
+        {
+            this.messageRepositoryMock = new Mock<IMessageRepository>();
+
+            this.messageService = new MessageService(
+                messageRepository: this.messageRepositoryMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static string GetRandomValidText()
+        {
+            return new MnemonicString(
+                  wordCount: 20, 
+                  wordMinLength: 3,
+                  wordMaxLength: 3
+              ).GetValue();
+        }
+
+        private static Message CreateRandomMessage() =>
+            CreateMessageFiller().Create();
+
+        private static Filler<Message> CreateMessageFiller()
+        {
+            var filler = new Filler<Message>();
+
+            filler.Setup()
+                .OnProperty(message => message.Text).Use(GetRandomValidText)
+                .OnProperty(message => message.TelegramBotConfiguration).IgnoreIt()
+                .OnType<DateTimeOffset>().Use(GetRandomDateTimeOffset)
+                .OnType<DateTimeOffset?>().Use(GetRandomDateTimeOffset());
+
+            return filler;
+        }
+    }
+}
