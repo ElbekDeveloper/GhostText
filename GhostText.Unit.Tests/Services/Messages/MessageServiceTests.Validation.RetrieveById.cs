3fc52529bb3e2287be62283dc9d4ec431a1414c5
ElbekDeveloper <normurodovelbek0133@gmail.com>
2025-09-15 19:14:15 +0500

    ShouldThrowArgumentExceptionOnRetrieveByIdIfMessageIdIsInvalidAsync -> FAIL

---
+++ GhostText.Unit.Tests/Services/Messages/MessageServiceTests.Validation.RetrieveById.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using GhostText.Models;
+using Moq;
+
+namespace GhostText.Unit.Tests.Services.Messages
+{
+    public partial class MessageServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowArgumentExceptionOnRetrieveByIdIfMessageIdIsInvalidAsync()
+        {
+            // given
+            Guid invalidMessageId = Guid.Empty;
+
+            var expectedArgumentException =
+                new ArgumentException("Message id is required.");
+
+            // when
+            ValueTask<Message> retrieveMessageByIdTask = 
+                this.messageService.RetrieveMessageByIdAsync(invalidMessageId);
+
+            ArgumentException actualArgumentException =
+                await Assert.ThrowsAsync<ArgumentException>(
+                    retrieveMessageByIdTask.AsTask);
+
+            // then
+            actualArgumentException.Message.Should()
+                .BeEquivalentTo(expectedArgumentException.Message);
+
+            this.messageRepositoryMock.Verify(repository =>
+                repository.SelectMessageByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.messageRepositoryMock.VerifyNoOtherCalls();
+            this.dateTimeRepositoryMock.VerifyNoOtherCalls();
+        }
+    }
+}
