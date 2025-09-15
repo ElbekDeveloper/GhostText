b8270e7491f8b9a81f5797a1b670fbb24c054083
ElbekDeveloper <normurodovelbek0133@gmail.com>
2025-09-15 19:53:55 +0500

    ShouldThrowArgumentExceptionOnModifyIfInputIsNullAsync -> FAIL

---
+++ GhostText.Unit.Tests/Services/Messages/MessageServiceTests.Validations.Modify.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using GhostText.Models;
+using Moq;
+
+namespace GhostText.Unit.Tests.Services.Messages
+{
+    public partial class MessageServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowArgumentExceptionOnModifyIfInputIsNullAsync()
+        {
+            // given
+            Message nullMessage = null;
+
+            var expectedArgumentException =
+                new ArgumentException("Message is null.");
+
+            // when
+            ValueTask<Message> modifyMessageTask =
+                this.messageService.ModifyMessageAsync(nullMessage);
+
+            ArgumentException actualArgumentException = 
+                await Assert.ThrowsAsync<ArgumentException>(
+                    modifyMessageTask.AsTask);
+
+            // then
+            actualArgumentException.Message.Should().BeEquivalentTo(
+                expectedArgumentException.Message);
+
+            this.messageRepositoryMock.Verify(repository =>
+                repository.SelectMessageByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.messageRepositoryMock.Verify(repository =>
+                repository.UpdateMessageAsync(It.IsAny<Message>()),
+                    Times.Never);
+
+            this.messageRepositoryMock.VerifyNoOtherCalls();
+            this.dateTimeRepositoryMock.VerifyNoOtherCalls();
+        }
+    }
+}
